9a0fac312c2d96414be91b72edaaf84ad24174a8
Kseniya_Kara4eva <kseniya-kara4eva@yandex.ru>
2022-12-18 12:17:54 +0300

    Create homework_5-6.js

---
+++ homework_5-6.js
@@ -0,0 +1,17 @@
+//5.6
+//Задание 6
+
+
+let arr = [1, 1, 1, 1, 1]
+
+for(let i = 0; i < arr.length; i++) {
+   if(arr[i] !== arr[i+1]) {
+     console.log("false");
+     break
+     }
+   else {
+     console.log("true");
+     break
+     }
+}
+
